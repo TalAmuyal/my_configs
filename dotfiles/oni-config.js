b7f2ab6a611fbc928cb8d0f5519722045233fa16
Tal Amuyal <TalAmuyal@gmail.com>
2018-02-16 11:55:24 +0200

    Update Oni's config to version 0.3.0

--- dotfiles/oni-config.js
+++ dotfiles/oni-config.js
@@ -12,16 +12,41 @@ const activate = (oni) => {
     oni.input.bind("<f3>", "oni.editor.gotoDefinition")
     oni.input.bind("<f4>", "oni.editor.findAllReferences")
     oni.input.bind("<f5>", "language.formatter.formatDocument")
+
+    oni.input.bind("<f8>", "markdown.togglePreview")
+    oni.input.bind("<f9>", "sidebar.toggle")
 };
 
 module.exports = {
     activate,
+
+    "autoClosingPairs.default": [
+        { open: "{", close: "}" },
+        { open: "[", close: "]" },
+        { open: "(", close: ")" },
+        { open: '"', close: '"' },
+        { open: '\'', close: '\'' },
+        { open: '`', close: '`' },
+        { open: '<', close: '>' },
+    ],
+
     "oni.useDefaultConfig": false,
     "oni.loadInitVim": true,
     "editor.fontSize": "14px",
     "editor.fontFamily": "Fira code",
     "oni.hideMenu": true,
     "tabs.mode": "hidden",
-    "ui.colorscheme": "solarized"
+    "ui.colorscheme": "solarized",
+    "sidebar.plugins.enabled": true,
+    "sidebar.enabled": true,
+    "editor.split.mode": "oni",
+
+    /* Experimental features */
+    "experimental.markdownPreview.enabled": true,
+    "experimental.commandline.mode": true,
+    "experimental.commandline.icons": true,
+
+    "experimental.wildmenu.mode": true,
+    "experimental.welcome.enabled": false,
 }
 
