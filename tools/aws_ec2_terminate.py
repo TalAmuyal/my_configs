53541c91f8f5bd2047b41f8a1765dbe389b993ce
Tal Amuyal <TalAmuyal@gmail.com>
2025-10-02 17:19:19 +0200

    Make aws_ec2_terminate more command-like

--- tools/aws_ec2_terminate.py
+++ tools/aws_ec2_terminate.py
@@ -1,3 +1,11 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#   "typer",
+#   "boto3",
+# ]
+# ///
+
 import boto3
 import typer
 
@@ -12,6 +20,8 @@ def main(private_ip: str) -> None:
     instance_name = get_instance_name(instance)
     if typer.confirm(f"Are you sure you want to terminate {instance_name}?"):
         ec2_client.terminate_instances(InstanceIds=[instance["InstanceId"]])
+        print("Terminating...")
+        wait_for_termination()
         print("Terminated")
     else:
         print("Aborted")
@@ -41,6 +51,21 @@ def get_instance_name(instance) -> str:
             return tag["Value"]
 
     raise Exception(f"Name not found ({instance.tags})")
+
+
+def wait_for_termination(
+    ec2_client,
+    private_ip: str,
+) -> None:
+    waiter = ec2_client.get_waiter("instance_terminated")
+    waiter.wait(
+        Filters=[
+            {
+                "Name": "private-ip-address",
+                "Values": [private_ip],
+            },
+        ],
+    )
 
 
 if __name__ == "__main__":
